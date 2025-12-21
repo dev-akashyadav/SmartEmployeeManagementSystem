88058dd083f8d475cf97915a0bc430d7ec53c005
dev-akashyadav <ay248516@gmail.com>
2025-12-21 13:19:59 +0530

    initial project commit

---
+++ src/main/java/com/dev_akashyadav/SmartEmployeeManagementSystem/SmartEmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dev_akashyadav.SmartEmployeeManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmployeeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev_akashyadav/SmartEmployeeManagementSystem/SmartEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_akashyadav.SmartEmployeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
